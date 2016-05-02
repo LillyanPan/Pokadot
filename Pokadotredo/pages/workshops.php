cdea967e0e787c1a87bc3df78b09eff8ae40d6a1
jay tappen <fjt37@cornell.edu>
2016-05-01 23:30:32 -0400

    switch order of classes on workshops page, per client request

--- Pokadotredo/pages/workshops.php
+++ Pokadotredo/pages/workshops.php
@@ -76,19 +76,19 @@
         <div class="row">
             <div class="col-md-2 col-sm-1"></div>
             <div class="col-md-8 col-sm-10">
-                <h1 class="indexHeader"><span class="dot red"></span> Art - 1 year olds and up</h1>
+                <h1 class="indexHeader"><span class="dot blue"></span> Art - 5 to 105 years old</h1>
                 <?php
-                $studio_pictures = array( "../images/artdabblers1_small.jpg",
-                                          "../images/artdabblers2_small.jpg",
-                                          "../images/artdabblers3_small.jpg");
+                $studio_pictures = array( "../images/artkidtechs1_small.jpg",
+                                          "../images/artkidtechs2_small.jpg",
+                                          "../images/artkidtechs3_small.jpg");
                 include "../components/studiopictures.php";
                 ?>
             </div>
             <div class="col-md-2 col-sm-1"></div>
         </div>
 
-         <!-- SECTION END -->
-         <div class="row">
+        <!-- SECTION END -->
+        <div class="row">
             <div class="col-md-3 col-xs-3"></div>
             <div class="col-md-6 col-xs-6 endSection"></div>
             <div class="col-md-3 col-xs-3"></div>
@@ -110,7 +110,7 @@
         </div>
 
         <!-- SECTION END -->
-         <div class="row">
+        <div class="row">
             <div class="col-md-3 col-xs-3"></div>
             <div class="col-md-6 col-xs-6 endSection"></div>
             <div class="col-md-3 col-xs-3"></div>
@@ -120,18 +120,18 @@
         <div class="row">
             <div class="col-md-2 col-sm-1"></div>
             <div class="col-md-8 col-sm-10">
-                <h1 class="indexHeader"><span class="dot blue"></span> Art - 5 to 105 years old</h1>
+                <h1 class="indexHeader"><span class="dot red"></span> Art - 1 year olds and up</h1>
                 <?php
-                $studio_pictures = array( "../images/artkidtechs1_small.jpg",
-                                          "../images/artkidtechs2_small.jpg",
-                                          "../images/artkidtechs3_small.jpg");
+                $studio_pictures = array( "../images/artdabblers1_small.jpg",
+                                          "../images/artdabblers2_small.jpg",
+                                          "../images/artdabblers3_small.jpg");
                 include "../components/studiopictures.php";
                 ?>
             </div>
             <div class="col-md-2 col-sm-1"></div>
         </div>
 
-          <!-- SECTION END -->
+         <!-- SECTION END -->
          <div class="row">
             <div class="col-md-1 col-xs-1"></div>
             <div class="col-md-10 col-xs-10 endSection final"></div>
