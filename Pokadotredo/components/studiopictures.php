b49abe953512c3895519cb882b212a05956ac26e
jay tappen <fjt37@cornell.edu>
2016-05-02 00:42:37 -0400

    added skeleton code for adding and deleting images, still missing javascript (and ajax)

--- Pokadotredo/components/studiopictures.php
+++ Pokadotredo/components/studiopictures.php
@@ -1,8 +1,18 @@
 <?php
+    // this code needs to be modified to pull images from database instead of $studio_pictures parameter
     echo "<div class='col-sm-12 studio-picture-container'>\n";
+    if (!empty($_SESSION['user']))
+        include "../components/manage_image_group.php";
     foreach($studio_pictures as $pic) {
         $alt = pathinfo($pic)["filename"];
-        echo "<div class='studio-picture'><img class='studio-picture-img' src='$pic' alt='$alt'></div>\n";
+        echo "<div class='studio-picture'>\n";
+        if (!empty($_SESSION['user'])) {
+            // add overlay on images to delete them
+            // need to add js to make ajax call if this button clicked
+            echo "<div class='overlay-delete-image'>delete</div>";
+        }
+        echo "<img class='studio-picture-img' src='$pic' alt='$alt'>\n";
+        echo "</div>\n";
     }
     echo "</div>\n";
 ?>
