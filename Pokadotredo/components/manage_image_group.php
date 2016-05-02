ec09d46f9c997eeb5beef2d1dbd5a932c7f68148
Annie Cheng <ac962@cornell.edu>
2016-05-02 01:27:15 -0400

    Remove duplicate image group form

--- Pokadotredo/components/manage_image_group.php
+++ Pokadotredo/components/manage_image_group.php
@@ -1,3 +1,19 @@
+<!--
+    This file will have similar functionality to the Edit Album forms
+    we created in Project 3. It will also have similar form validation 
+    implemented as in the contact_form.php and sign_up_form.php file. 
+    This form will allow the admin, when logged in, to add/edit/delete images in
+    groups on the website. This form will only be visible if the user is logged in as 
+    an admin. We will use regexes and filter input functions to implement form 
+    validation. We will also prevent code injection by stripping HTML tags and 
+    elements. Furthermore, we will integrate the validation feedback by detecting 
+    which fields have invalid inputs and display appropriate feedback messages. 
+    What the specific validation code implementation will look like is similar to 
+    what is currently in the file contact_form.php. We will use php code to validate 
+    inputs, interact with the database, and upload files in a similar way to what we 
+    did in Project 3. 
+-->
+
 <form id="add-image-form" method="POST" action="#" enctype="multipart/form-data" style="margin-bottom: 10px">
     Add an image to this group: <br>
     <input type="file" name="image_upload" id="image_upload" required title="Please select a file to upload" style="display:inline-block"><br>
