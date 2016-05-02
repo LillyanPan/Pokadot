35887056c94fd9ab696fa35c09317492e02ddadb
Annie Cheng <ac962@cornell.edu>
2016-05-02 00:26:45 -0400

    Show calendar for sign up only for now

--- Pokadotredo/components/calendar.php
+++ Pokadotredo/components/calendar.php
@@ -1,3 +1,5 @@
 <?php
-
+	echo '<div class="google-calendar-container">
+			<iframe src="https://calendar.google.com/calendar/embed?src=8ck8ha14j21jmdhrtd43a6q6rg%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+		</div>'
 ?>
