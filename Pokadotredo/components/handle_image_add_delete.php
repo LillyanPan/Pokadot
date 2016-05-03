50d31995b9f01d8f846b5688e2d81467fda28df7
jay tappen <fjt37@cornell.edu>
2016-05-02 20:39:10 -0400

    added file to manage adding and deleting images

---
+++ Pokadotredo/components/handle_image_add_delete.php
@@ -0,0 +1,10 @@
+<?php
+    /*
+    This file will be included at the top of any page with image groups,
+    and will check the POST data to see if a request was made to add or 
+    remove an image. If so, it will check that the user is, in fact, 
+    logged in, and then validate inputs and perform the requested
+    operation, updating the database as necessary to keep track of images 
+    and image groups.
+    */
+?>
