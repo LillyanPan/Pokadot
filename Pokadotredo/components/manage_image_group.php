57c532a99611091a603bee696af6fa9ff39a46e8
jay tappen <fjt37@cornell.edu>
2016-05-12 22:52:58 -0400

    studio picture presentation with database calls and bootstrap. also image add form

--- Pokadotredo/components/manage_image_group.php
+++ Pokadotredo/components/manage_image_group.php
@@ -1,23 +1,7 @@
-<!--
-    This file will have similar functionality to the Edit Album forms
-    we created in Project 3. It will also have similar form validation 
-    implemented as in the contact_form.php and sign_up_form.php file. 
-    This form will allow the admin, when logged in, to add/edit/delete images in
-    groups on the website. This form will only be visible if the user is logged in as 
-    an admin. We will use regexes and filter input functions to implement form 
-    validation. We will also prevent code injection by stripping HTML tags and 
-    elements. Furthermore, we will integrate the validation feedback by detecting 
-    which fields have invalid inputs and display appropriate feedback messages. 
-    What the specific validation code implementation will look like is similar to 
-    what is currently in the file contact_form.php. We will use php code to validate 
-    inputs, interact with the database, and upload files in a similar way to what we 
-    did in Project 3. 
--->
-
-<!-- rough form for now, not very pretty yet on the site, but it gives the general idea -->
 <form class="add-image-form" method="POST" action="#" enctype="multipart/form-data" style="margin-bottom: 10px">
     Add an image to this group: <br>
+    <input type="hidden" name="group_id" value=<?php echo "'$group_id'" ?> >
     <input type="file" name="image_upload" required title="Please select a file to upload" style="display:inline-block"><br>
     <input type="text" name="image_desc" required maxlength="40" placeholder="Description" title="Please include a short desciption of the image" style="display:inline-block">
-    <input type="submit" value="Add Image" style="display:inline-block">
+    <input type='submit' value='Add Image' style='display:inline-block'?>
 </form>
--- Pokadotredo/components/studiopictures.php
+++ Pokadotredo/components/studiopictures.php
@@ -1,18 +1,31 @@
-<?php
-    // this code will be modified to pull images from database instead of $studio_pictures parameter
-    echo "<div class='col-sm-12 studio-picture-container'>\n";
-    if (!empty($_SESSION['user']))
-        include "../components/manage_image_group.php";
-    foreach($studio_pictures as $pic) {
-        $alt = pathinfo($pic)["filename"];
-        echo "<div class='studio-picture'>\n";
-        if (!empty($_SESSION['user'])) {
-            // add button on images to give the option to delete them.
-            // this button needs to be put in a form so that it can be made a submit button.
-            echo "<div class='overlay-delete-image'>delete</div>\n";
+<div class="row">
+    <div class="col-md-2 col-sm-1"></div>
+    <div class="col-md-8 col-sm-10 studio-picture-container">
+    <?php
+        $group_id = $group_id; // this parameter should be set before including this file
+        if (!empty($_SESSION['user']))
+            include "../components/manage_image_group.php";
+        $stmt = $pdo->prepare(
+           "SELECT filepath, description
+            FROM images LEFT JOIN images_in_groups
+                ON images.image_id=images_in_groups.image_id
+            WHERE images_in_groups.group_id=:groupid");
+        $stmt->bindParam(":groupid", $group_id);
+        $stmt->execute();
+        $pictures = $stmt->fetchAll();
+        foreach($pictures as $pic) {
+            $src = $pic['filepath'];
+            $alt = $pic["description"];
+            echo "<div class='col-md-4 col-sm-12'>\n";
+            if (!empty($_SESSION['user'])) {
+                // add button on images to give the option to delete them.
+                // this button needs to be put in a form so that it can be made a submit button.
+                echo "<div class='overlay-delete-image'>delete</div>\n";
+            }
+            echo "<img class='studio-picture-img' src='$src' alt='$alt'>\n";
+            echo "</div>\n";
         }
-        echo "<img class='studio-picture-img' src='$pic' alt='$alt'>\n";
-        echo "</div>\n";
-    }
-    echo "</div>\n";
-?>
+    ?>
+    </div>
+    <div class="col-md-2 col-sm-1"></div>
+</div>
