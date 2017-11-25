184f571fcdb402e242781902783e831b9af2f5a5
fjt37 <fjt37@cornell.edu>
2017-11-25 11:23:31 -0500

    changed contact and footer

--- components/footer.php
+++ components/footer.php
@@ -5,10 +5,10 @@
             <div class="col-md-3 col-xs-3"></div>
              <div class="col-md-6 col-xs-6">
                 <div class="cod-md-12">
-                	<a class="signup-button" href=<?php echo "'{$path_to_root}pages/signup.php'"?>>Sign up for a workshop!</a>
-                    <p class="footer">gopokadot@gmail.com</p>
+                	<a class="signup-button" href="mailto:gopokadot@gmail.com">gopokadot@gmail.com</a>
+                    <!-- <p class="footer">gopokadot@gmail.com</p> -->
                     <p class="footer">Concord, MA</p>
-                </div> 
+                </div>
              </div>
             <div class="col-md-3 col-xs-3"></div>
         </div>
--- pages/contactus.php
+++ pages/contactus.php
@@ -32,55 +32,16 @@
         </div>
         <!-- SECTION END -->
 
-        <!-- <div class="row">
+        <div class="row">
             <div class="col-md-3 col-sm-3"></div>
             <div class="col-md-6 col-sm-6">
                 <div class="col-md-12 col-sm-12">
-                    <h1 class="indexHeader">Call Us</h1>
-                    <p class="center">857-488-6061</p>
+                    <h1 class="indexHeader">Send us an email to get in touch!</h1>
                 </div>
             </div>
             <div class="col-md-3 col-sm-3"></div>
-        </div> -->
-
-         <!-- SECTION END -->
-        <div class="row">
-            <div class="col-md-3 col-xs-3"></div>
-            <div class="col-md-6 col-xs-6 endSection"></div>
-            <div class="col-md-3 col-xs-3"></div>
-        </div>
-        <!-- SECTION END -->
-
-
-        <div class="row">
-            <div class="col-md-3 col-sm-3"></div>
-            <div class="col-md-6 col-sm-6">
-                <h1 class="indexHeader">Email Us</h1>
-                <form id="contact-form" class="form" method="post" action="contactForm.php">
-                    <div class="soft-text">All fields required.</div>
-                    <div class="multifield-line">
-                        <input id="contact-name-field" type="text" required name="name" title="Please enter your name" placeholder="Name">
-                        <input id="contact-email-field" type="text" required name="email" title="Please enter your email" placeholder="Email">
-                    </div>
-                    <input id="contact-subject-field" class="wide-text" type="text" required name="subject" title="Please enter a subject for your message." placeholder="Please enter a subject for your message.">
-                    <textarea id="contact-msg-field" class="wide-text" form="contact-form" required name="msg" rows=8 title="Please enter a body for your message." placeholder="Please enter a body for your message."></textarea>
-                    <input id="add-mailing-list" name="add-mailing-list" type="checkbox">
-                    <label for="add-mailing-list">Add me to the email list</label>
-                    <input id="contactButton" type="submit" value="Send Message">
-             </form>
-            </div>
-            <div class="col-md-3 col-sm-3"></div>
-        </div>
-        <div class="row">
-            <div id="form-messages" class="text-center col-md-6 col-sm-6 col-md-offset-3"></div>
         </div>
 
-         <!-- SECTION END -->
-         <div class="row">
-            <div class="col-md-1 col-xs-1"></div>
-            <div class="col-md-10 col-xs-10 endSection final"></div>
-            <div class="col-md-1 col-xs-1"></div>
-        </div>
         <!-- SECTION END -->
 
         <?php include "../components/footer.php" ?>
